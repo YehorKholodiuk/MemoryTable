0ba132e929566ff031365d950823b068cfcaf5d0
ASUS <Yehor Kholodiuk>
2021-11-20 16:43:06 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import { shuffle, chunk } from 'lodash';
+import { nanoid } from 'nanoid';
 import './App.css';
-
-function App() {
+import { useState } from 'react';
+function App(){
+  const side = 4;
+  let variants = [];
+  for (let i = 1; i <= (side * side)/2; i++){
+    variants.push(i);
+  }
+  variants = [...variants,...variants];
+  variants = shuffle(variants).map(el => ({
+    open:false,
+    value:el,
+    id: nanoid()
+      })
+  );
+  variants = chunk (variants,side);
+  console.log(variants);
+  const [gameField,setGameField] = useState(variants);
+  const openClick = (id) =>{
+    const updatedGameField = gameField.map(row =>row.map (col => col.id === id ? {
+      ...col,
+      open:true
+    }:col ));
+    setGameField(updatedGameField);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <table border={1}>
+          <tbody>
+          {gameField.map((row, rowIndex) => (
+              <tr key={rowIndex}>
+                {row.map((col, colIndex) => (
+                    <td key={colIndex}>{col.open ? col.value :
+                        <button onClick={() => openClick(col.id)}>Open</button>}</td>
+                ))}
+              </tr>
+          ))}
+          </tbody>
+        </table>
+      </div>
   );
 }
-
 export default App;
